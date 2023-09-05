092c70166350586a8981ab6fbf58fb7b7b208fd6
alexandrumucenic <alexmucenic6@gmail.com>
2023-09-05 13:33:36 +0300

    implemented adding event logic

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import {
   RouterProvider,
   createBrowserRouter,
 } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
+import { QueryClientProvider } from '@tanstack/react-query';
+//components
 import Events from './components/Events/Events.jsx';
 import EventDetails from './components/Events/EventDetails.jsx';
 import NewEvent from './components/Events/NewEvent.jsx';
 import EditEvent from './components/Events/EditEvent.jsx';
+//utils
+import { queryClient } from './util/http.js';
 
 const router = createBrowserRouter([
   {
@@ -37,8 +39,6 @@ const router = createBrowserRouter([
     ],
   },
 ]);
-
-const queryClient = new QueryClient();
 
 function App() {
   return (
--- src/components/Events/NewEvent.jsx
+++ src/components/Events/NewEvent.jsx
@@ -1,25 +1,54 @@
 import { Link, useNavigate } from 'react-router-dom';
-
+import { useMutation } from '@tanstack/react-query';
+//components
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
+//utils
+import { queryClient, createNewEvent } from '../../util/http.js';
 
 export default function NewEvent() {
   const navigate = useNavigate();
 
-  function handleSubmit(formData) {}
+  const { mutate, isPending, isError, error } = useMutation({
+    mutationFn: createNewEvent,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['events'] });
+      navigate('/events');
+    },
+  });
+
+  function handleSubmit(formData) {
+    mutate({
+      event: formData,
+    });
+  }
 
   return (
     <Modal onClose={() => navigate('../')}>
       <EventForm onSubmit={handleSubmit}>
-        <>
-          <Link to="../" className="button-text">
-            Cancel
-          </Link>
-          <button type="submit" className="button">
-            Create
-          </button>
-        </>
+        {isPending && 'Submitting...'}
+
+        {!isPending && (
+          <>
+            <Link to="../" className="button-text">
+              Cancel
+            </Link>
+            <button type="submit" className="button">
+              Create
+            </button>
+          </>
+        )}
       </EventForm>
+      {isError && (
+        <ErrorBlock
+          title="Failed to create event"
+          message={
+            error.info?.message ||
+            'Failed to create event. Please check your inputs and try again.'
+          }
+        />
+      )}
     </Modal>
   );
 }
